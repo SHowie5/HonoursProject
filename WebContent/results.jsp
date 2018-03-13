c96d3232576fc0e9dd87d09cbf9e0c4ee1ba1ef1
SHowie5 <scotthowie5@gmail.com>
2018-03-13 17:55:04 +0000

    Work done on texture histogram

--- WebContent/results.jsp
+++ WebContent/results.jsp
@@ -7,6 +7,7 @@
 
 
 
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -43,5 +44,7 @@
 	<%
 		}
 	%>
+	
+	
 </body>
 </html>
--- src/Upload/textureDirectionHistogram.java
+++ src/Upload/textureDirectionHistogram.java
@@ -1,21 +1,27 @@
 package Upload;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.ImagingOpException;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
 
 import com.sun.org.apache.xml.internal.utils.URI;
 
+import stores.UploadStore;
+
 public class textureDirectionHistogram {
 	
+	UploadStore us = new UploadStore();
 	BufferedImage bImage = null;
 	double imageSize;
 	
 	public void readImage(String image) {
-
+		
 		// Checks if input is from local file or URL
 		boolean check = checkInput(image);
 
@@ -36,7 +42,29 @@ public class textureDirectionHistogram {
 				e.printStackTrace();
 			}
 		}
+		
+		// Edge detection code
+		
+		for (int x = 0; x < bImage.getWidth(); x++) {
+			for (int y = 0; y < bImage.getHeight(); y++) {
+				int clr = bImage.getRGB(x, y);
+				int red = (clr & 0x00ff0000) >> 16;
+				int green = (clr & 0x0000ff00) >> 8;
+				int blue = clr & 0x000000ff;
 
+				float luminance = (red * 0.2126f + green * 0.7152f + blue * 0.0722f) / 255;
+				
+				// choose brightness threshold as appropriate:
+				if (luminance >= 0.5f) {
+				    // bright color
+					bImage.setRGB(x, y, 255);
+				} else {
+				    // dark color
+					bImage.setRGB(x, y, 0);
+				}
+			}
+		}
+		
 	}
 	
 	
